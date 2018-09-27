17116f58cd8d437aa7d129a98bed1e040b2f11bb
TobiasEmil <tobias@protofi.dk>
2018-09-27 17:43:36 +0200

    household simple initialization functionalitity

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,7 @@
 const auth = firebase.auth()
+const db = firebase.firestore();
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
 
 document.getElementById('signup-form').addEventListener('submit', (event) => {
     event.preventDefault();
@@ -21,7 +24,7 @@ document.getElementById('signin-form').addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('signin')
 
-    const email = "tobiasharbo@gmail.com";
+    const email = "tobias@mail.com";
     const password = "notsecure123";
 
     auth
@@ -45,7 +48,6 @@ document.getElementById('delete-user').addEventListener('click', event => {
     }).catch(function(error) {
         console.log(error)
     });
-
 })
 
 document.getElementById('signout').addEventListener('click', (event) => {
@@ -54,5 +56,36 @@ document.getElementById('signout').addEventListener('click', (event) => {
 })
 
 auth.onAuthStateChanged(user => {
+
+    if(!user)
+    {
+        console.log('Signed out');
+        return;
+    }
+
     console.log(user)
+})
+
+const form = document.getElementById('init-household-form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = form.getElementsByClassName('household-name')[0].value;
+    console.log(name)
+
+    const user = auth.currentUser;
+
+    db.collection("households").add({
+        name : name,
+    }).then((household) => {
+        db.collection("households").doc(household.id).collection('members').doc(user.uid).set({
+            uid : user.uid,
+            name : user.displayName
+        });
+
+        db.collection('households').doc(household.id).collection('members').get()
+        .then(members => {
+            console.log(members.docs.map(doc => doc.data()))
+        })
+    })
 })
